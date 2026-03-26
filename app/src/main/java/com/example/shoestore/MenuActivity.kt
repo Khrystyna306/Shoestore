37945026bfce2cd9def8f22bb8a0881f60f3bc0e
Khrystyna306 <hrystyna.nykolaychuk@kpk-lp.com.ua>
2026-03-26 13:08:49 +0200

    stage 3 : MenuActivity

--- app/src/main/java/com/example/shoestore/MenuActivity.kt
+++ app/src/main/java/com/example/shoestore/MenuActivity.kt
@@ -1,20 +1,36 @@
 package com.example.shoestore
 
+import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var prefs: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        prefs = getSharedPreferences("UserData", MODE_PRIVATE)
+
+        val tvWelcome = findViewById<TextView>(R.id.tvWelcome)
+        val btnLogout = findViewById<Button>(R.id.btnLogout)
+
+        // Отримуємо ім'я і прізвище
+        val firstName = prefs.getString("firstName", "")
+        val lastName = prefs.getString("lastName", "")
+
+        tvWelcome.text = "Привіт, $firstName $lastName!"
+
+        // Logout
+        btnLogout.setOnClickListener {
+            prefs.edit().putBoolean("isAuthorized", false).apply()
+
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
         }
     }
 }
